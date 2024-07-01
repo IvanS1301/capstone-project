c1c5d08e29983ec5bafc6bfd6996111ebddd2e9f
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-01 21:30:07 +0800

    Update AssignPage.jsx

--- frontend/src/pages/admin/AssignPage.jsx
+++ frontend/src/pages/admin/AssignPage.jsx
@@ -7,7 +7,7 @@ const AssignPage = ({ leadId, onLeadUpdate }) => {
 
     useEffect(() => {
         const fetchUsers = async () => {
-            const response = await fetch('/api/userLG');
+            const response = await fetch('http://localhost:4000/api/userLG');
             const json = await response.json();
 
             if (response.ok) {
