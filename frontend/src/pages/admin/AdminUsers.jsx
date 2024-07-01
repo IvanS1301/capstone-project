b9c2806c2cf6a2f203679aa0e7ab8c548b264344
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-01 22:10:36 +0800

    Update AdminUsers.jsx

--- frontend/src/pages/admin/AdminUsers.jsx
+++ frontend/src/pages/admin/AdminUsers.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import { URL } from "@utils/URL";
 
 /** --- MATERIAL UI --- */
 import { CircularProgress } from "@mui/material";
@@ -19,7 +20,7 @@ const AdminUsers = () => {
 
   const fetchUsers = useCallback(async () => {
     try {
-      const response = await fetch('http://localhost:4000/api/userLG');
+      const response = await fetch(`${URL}/api/userLG`);
       const json = await response.json();
 
       if (response.ok) {
