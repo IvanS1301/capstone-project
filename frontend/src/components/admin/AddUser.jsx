868b07661ab6385cf9b4003dd531fba4252d8a4f
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-01 23:28:49 +0800

    Update AddUser.jsx

--- frontend/src/components/admin/AddUser.jsx
+++ frontend/src/components/admin/AddUser.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { URL } from "../../../utils/URL";
+import { URL } from "../../utils/URL";
 
 /** --- MATERIAL UI --- */
 import { Box, Button, TextField, FormControl, InputLabel, Select as MuiSelect, MenuItem, Modal, CircularProgress } from '@mui/material';
