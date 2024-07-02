a7c084584ceaeaadffebea907554f83988b8b98e
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-02 18:06:33 +0800

    updated

--- backend/models/inventoryModel.js
+++ backend/models/inventoryModel.js
@@ -26,6 +26,12 @@ const inventorySchema = new mongoose.Schema({
         of: Number,
         required: true
     },
+    teamBookedCounts: {
+        type: Map,
+        of: Number,
+        required: true,
+        default: { 'Team A': 0, 'Team B': 0, 'Team C': 0 }
+    },
     updatedAt: {
         type: Date,
         default: () => Date.now(),
