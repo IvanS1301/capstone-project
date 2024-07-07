a471ccadb265f6fcd9465740fee9656a2a5592e1
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-07 12:55:13 +0800

    updated

--- frontend/src/components/profile/UpdateUserLG.jsx
+++ frontend/src/components/profile/UpdateUserLG.jsx
@@ -128,6 +128,7 @@ const UpdateUserLG = ({ userId, onUserUpdate }) => {
             name="gender"
             value={userData.gender}
             onChange={handleChange}
+            label="Gender"
           >
             <MenuItem value=""><em>Choose One</em></MenuItem>
             <MenuItem value="Male">Male</MenuItem>
