5bf44c307b3b2b17cd110d37668b8554dcdfb4a5
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-11 13:25:10 +0800

    updated

--- frontend/src/components/admin/LeadGenStats.jsx
+++ frontend/src/components/admin/LeadGenStats.jsx
@@ -86,6 +86,9 @@ const LeadGenStats = ({ leadGenStats }) => {
                     color: "#e0e0e0",
                     fontSize: "18px",
                   },
+                  "& .MuiDataGrid-sortIcon": {
+                    color: "#ffffff !important", // Change sort icon color to white
+                  },
                   "& .MuiDataGrid-virtualScroller": {
                     backgroundColor: "#d1d5db",
                     fontSize: "17px",
