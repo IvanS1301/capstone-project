dfe99305397463b28924eaff2191ddea53b39ef0
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-23 15:58:55 +0800

    updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './global.css'
-import App from './App'
-import { LeadsContextProvider } from './context/LeadsContext'
-import { AuthContextProvider } from './context/AuthContext'
-import { UsersContextProvider } from './context/UsersContext'
-import { EmailsContextProvider } from './context/EmailsContext'
-import { ServicesContextProvider } from './context/ServicesContext'
-import { AdminContextProvider } from './context/AdminContext'
-import { LeadgenContextProvider } from './context/LeadgenContext'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './global.css';
+import App from './App';
+import { LeadsContextProvider } from './context/LeadsContext';
+import { AuthContextProvider } from './context/AuthContext';
+import { UsersContextProvider } from './context/UsersContext';
+import { EmailsContextProvider } from './context/EmailsContext';
+import { ServicesContextProvider } from './context/ServicesContext';
+import { AdminContextProvider } from './context/AdminContext';
+import { LeadgenContextProvider } from './context/LeadgenContext';
+import { TemplatesContextProvider } from './context/TemplatesContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
@@ -20,7 +22,9 @@ root.render(
             <UsersContextProvider>
               <LeadgenContextProvider>
                 <LeadsContextProvider>
-                  <App />
+                  <TemplatesContextProvider>
+                    <App />
+                  </TemplatesContextProvider>
                 </LeadsContextProvider>
               </LeadgenContextProvider>
             </UsersContextProvider>
@@ -29,4 +33,4 @@ root.render(
       </AdminContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
-)
+);
