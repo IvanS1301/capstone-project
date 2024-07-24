b5151a3c5b1746dcf908af822b05357966ccfec8
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-24 09:36:34 +0800

    updated

--- frontend/src/components/agent/AGDashboardTabs.jsx
+++ frontend/src/components/agent/AGDashboardTabs.jsx
@@ -144,7 +144,7 @@ const AGDashboardTabs = ({ bookedUnits, unassignedLeads }) => {
                                 letterSpacing: '3px',
                                 backgroundColor: 'hsl(261deg 80% 48%)',
                                 color: 'hsl(0, 0%, 100%)',
-                                boxShadow: '0px 7px 29px 0px rgb(93 24 220)',
+                                boxShadow: '0px 0px 9px 0px rgb(93 24 220)',
                             }
                         }}
                     >
