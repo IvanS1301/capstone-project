bae249b0217040782c21cbc1ca60e2148cdf8c02
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-25 14:28:47 +0800

    updated

--- frontend/src/components/admin/EmailList.jsx
+++ frontend/src/components/admin/EmailList.jsx
@@ -7,7 +7,7 @@ import { useAuthContext } from "../../hooks/useAuthContext";
 
 /** --- MATERIAL UI --- */
 import { Box, Button, Snackbar, IconButton, Modal, CircularProgress, Typography } from "@mui/material";
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { DataGrid } from "@mui/x-data-grid";
 import { Delete, Visibility } from '@mui/icons-material';
 import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
@@ -17,6 +17,9 @@ import { Link } from 'react-router-dom';
 
 /** --- TIME AND DATE FORMAT --- */
 import moment from 'moment'
+
+/** --- IMPORT CHART --- */
+import CustomToolbar from '../Chart/CustomToolbar';
 
 const EmailList = ({ emails, userlgs, onEmailDelete }) => {
     const { dispatch } = useEmailsContext();
@@ -216,7 +219,7 @@ const EmailList = ({ emails, userlgs, onEmailDelete }) => {
                         color: `#111827 !important`,
                     },
                     "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-                        color: `#111827 !important`,
+                        color: `#e0e0e0 !important`,
                         fontWeight: "500"
                     },
                 }}
@@ -235,7 +238,7 @@ const EmailList = ({ emails, userlgs, onEmailDelete }) => {
                     }}
                     selectionModel={selectedRows}
                     slots={{
-                        toolbar: GridToolbar,
+                        toolbar: CustomToolbar,
                     }}
                     getRowId={row => row._id}
                 />
