6f931f07a7fcec28e08e831dbc967ab23226a36b
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-26 13:31:10 +0800

    updated

--- backend/models/statusModel.js
+++ backend/models/statusModel.js
@@ -15,6 +15,11 @@ const statusSchema = new Schema({
         type: String,
         enum: ["Start Shift", "End Shift", "First Break", "Lunch", "Team Meeting", "Coaching"],
     },
+    userLG_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'UserLG', // Ensure this matches your UserLG model name
+        required: true
+    },
 }, { timestamps: true })
 
 module.exports = mongoose.model('Employee Status', statusSchema)
