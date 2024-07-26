942103c2e0e611b7ea22bf4558ee60f8831208a2
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-26 14:18:15 +0800

    updated

--- frontend/src/components/agent/AgentUpdateForm.jsx
+++ frontend/src/components/agent/AgentUpdateForm.jsx
@@ -302,6 +302,7 @@ const AgentUpdateForm = ({ unassignedId, onLeadUpdate }) => {
             <Modal
                 open={openSuccessModal}
                 onClose={handleCloseSuccessModal}
+                className="bounce-in-modal"
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
