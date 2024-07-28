aa9ea5df7a55a3dc40bffe6467b1b855cc0d703c
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-28 11:05:58 +0800

    updated

--- frontend/src/components/admin/AdminNavbar.jsx
+++ frontend/src/components/admin/AdminNavbar.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
-import { URL } from "../../utils/URL";
 import { Link } from "react-router-dom";
+import { URL } from "../../utils/URL";
 
 /** --- MATERIAL UI --- */
-import { Box, IconButton, Badge, Menu, MenuItem, Typography } from "@mui/material";
+import { Box, Button, IconButton, Badge, Menu, MenuItem, Typography, Avatar, Divider, Paper, Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
 
 /** --- MATERIAL UI ICONS --- */
 import InputBase from "@mui/material/InputBase";
-import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 import SearchIcon from "@mui/icons-material/Search";
 
 /** --- IMPORT HOOKS --- */
@@ -19,6 +19,9 @@ const AdminNavbar = ({ onSearch }) => {
     const [notifications, setNotifications] = useState([]);
     const [anchorEl, setAnchorEl] = useState(null);
     const [profileAnchorEl, setProfileAnchorEl] = useState(null);
+    const [filter, setFilter] = useState("all");
+    const [openDialog, setOpenDialog] = useState(false);
+    const [selectedNotification, setSelectedNotification] = useState(null);
 
     const { userLG } = useAuthContext();
 
@@ -39,7 +42,7 @@ const AdminNavbar = ({ onSearch }) => {
     const handleSearch = (event) => {
         const query = event.target.value;
         setSearchQuery(query);
-        onSearch(query); // Call the onSearch function passed from AdminLeads
+        onSearch(query);
     };
 
     const handleNotificationClick = (event) => {
@@ -50,9 +53,9 @@ const AdminNavbar = ({ onSearch }) => {
         setAnchorEl(null);
     };
 
-    const handleMarkAsRead = async (id) => {
+    const handleMarkAsRead = async (notification) => {
         try {
-            const response = await fetch(`${URL}/api/notifications/${id}/read`, {
+            const response = await fetch(`${URL}/api/notifications/${notification._id}/read`, {
                 method: 'PATCH',
                 headers: {
                     'Content-Type': 'application/json'
@@ -60,7 +63,9 @@ const AdminNavbar = ({ onSearch }) => {
             });
 
             if (response.ok) {
-                setNotifications(notifications.map(n => n._id === id ? { ...n, isRead: true } : n));
+                setNotifications(notifications.map(n => n._id === notification._id ? { ...n, isRead: true } : n));
+                setSelectedNotification(notification);
+                setOpenDialog(true);
             } else {
                 console.error('Failed to mark notification as read');
             }
@@ -77,15 +82,20 @@ const AdminNavbar = ({ onSearch }) => {
         setProfileAnchorEl(null);
     };
 
+    const handleDialogClose = () => {
+        setOpenDialog(false);
+        setSelectedNotification(null);
+    };
+
     const unreadCount = notifications.filter(n => !n.isRead).length;
+    const filteredNotifications = filter === "all" ? notifications : notifications.filter(n => !n.isRead);
 
     return (
         <Box display="flex" justifyContent="space-between" p={2}>
-            {/* SEARCH BAR */}
             <Box
                 display="flex"
                 backgroundColor="#111827"
-                borderRadius="20px"
+                borderRadius="25px"
                 sx={{ width: "60%", maxWidth: "400px", p: "6px" }}
             >
                 <InputBase
@@ -99,31 +109,141 @@ const AdminNavbar = ({ onSearch }) => {
                 </IconButton>
             </Box>
 
-            {/* ICONS */}
             <Box display="flex">
-                <IconButton sx={{ color: "#111827" }} onClick={handleNotificationClick}>
-                    <Badge badgeContent={unreadCount} color="error">
-                        <NotificationsOutlinedIcon />
+                <IconButton
+                    sx={{
+                        color: "#111827",
+                        fontSize: 35, // Adjust the icon size
+                        '& .MuiBadge-dot': {
+                            width: 10,
+                            height: 10,
+                            borderRadius: '50%',
+                        },
+                    }}
+                    onClick={handleNotificationClick}
+                >
+                    <Badge
+                        badgeContent={unreadCount}
+                        color="error"
+                        sx={{
+                            '.MuiBadge-dot': {
+                                width: 14, // Adjust the size of the badge
+                                height: 14,
+                            },
+                        }}
+                    >
+                        <NotificationsRoundedIcon
+                            sx={{
+                                fontSize: 'inherit', // Ensure the icon inherits the size from IconButton
+                            }}
+                        />
                     </Badge>
                 </IconButton>
+
                 <Menu
                     anchorEl={anchorEl}
                     open={Boolean(anchorEl)}
                     onClose={handleNotificationClose}
+                    PaperProps={{
+                        style: {
+                            width: '550px',
+                            maxHeight: '950px',
+                            backgroundColor: '#212e4a',
+                            borderRadius: "20px"
+                        },
+                    }}
                 >
-                    {notifications.map((notification) => (
-                        <MenuItem
-                            key={notification._id}
-                            onClick={() => handleMarkAsRead(notification._id)}
-                            sx={{ backgroundColor: notification.isRead ? 'inherit' : '#f0f0f0' }}
-                        >
-                            {notification.message}
-                        </MenuItem>
-                    ))}
+                    <Box p={2} backgroundColor="#212e4a">
+                        <Typography variant="h6" gutterBottom sx={{ fontSize: '30px', fontWeight: 'bold', color: '#e0e0e0', ml: "5px" }}>
+                            Notifications
+                        </Typography>
+                        <Box display="flex" justifyContent="center" mb={3}>
+                            <Button
+                                variant={filter === 'all' ? 'contained' : 'outlined'}
+                                onClick={() => setFilter('all')}
+                                sx={{
+                                    textTransform: 'none',
+                                    backgroundColor: filter === 'all' ? '#3b5998' : 'transparent',
+                                    color: filter === 'all' ? '#fff' : '#3b5998',
+                                    borderColor: '#3b5998',
+                                    mr: 1,
+                                    '&:hover': {
+                                        backgroundColor: filter === 'all' ? '#3b5998' : '#f0f0f0',
+                                        borderColor: '#3b5998'
+                                    }
+                                }}
+                            >
+                                All
+                            </Button>
+                            <Button
+                                variant={filter === 'unread' ? 'contained' : 'outlined'}
+                                onClick={() => setFilter('unread')}
+                                sx={{
+                                    textTransform: 'none',
+                                    backgroundColor: filter === 'unread' ? '#3b5998' : 'transparent',
+                                    color: filter === 'unread' ? '#fff' : '#3b5998',
+                                    borderColor: '#3b5998',
+                                    '&:hover': {
+                                        backgroundColor: filter === 'unread' ? '#3b5998' : '#f0f0f0',
+                                        borderColor: '#3b5998'
+                                    }
+                                }}
+                            >
+                                Unread
+                            </Button>
+                        </Box>
+                        <Divider sx={{ mt: 2, mb: 2, bgcolor: "#e0e0e0" }} />
+                        {filteredNotifications.map((notification) => (
+                            <MenuItem
+                                key={notification._id}
+                                onClick={() => handleMarkAsRead(notification)}
+                                sx={{
+                                    display: 'block',
+                                    whiteSpace: 'normal',
+                                    mb: 1
+                                }}
+                            >
+                                <Paper sx={{ p: 2, width: '100%', backgroundColor: '#041926', }} elevation={notification.isRead ? 1 : 4}>
+                                    <Box display="flex" alignItems="center" mb={2}>
+                                        <Avatar sx={{ bgcolor: '#3e4396', mr: 2 }}>{notification.message.charAt(0)}</Avatar>
+                                        <Box>
+                                            <Typography variant="body1" sx={{ color: '#94e2cd', fontWeight: notification.isRead ? 'normal' : 'bold' }}>
+                                                {notification.message}
+                                            </Typography>
+                                            <Typography variant="caption" color="#e0e0e0">
+                                                {new Date(notification.createdAt).toLocaleString()}
+                                            </Typography>
+                                        </Box>
+                                    </Box>
+                                    <Divider sx={{ mt: 1, mb: 1, bgcolor: "#e0e0e0" }} />
+                                    <Typography variant="caption" color="#e0e0e0" sx={{ display: 'block', textAlign: 'right' }}>
+                                        {notification.isRead ? 'Read' : 'Unread'}
+                                    </Typography>
+                                </Paper>
+                            </MenuItem>
+                        ))}
+                    </Box>
                 </Menu>
-                <IconButton sx={{ color: "#111827" }} onClick={handleProfileMenuOpen}>
-                    <PersonOutlinedIcon />
+
+                <IconButton
+                    sx={{
+                        color: "#111827",
+                        fontSize: 35, // Adjust the icon size
+                        '& .MuiBadge-dot': {
+                            width: 10,
+                            height: 10,
+                            borderRadius: '50%',
+                        },
+                    }}
+                    onClick={handleProfileMenuOpen}
+                >
+                    <AccountCircleRoundedIcon
+                        sx={{
+                            fontSize: 'inherit', // Ensure the icon inherits the size from IconButton
+                        }}
+                    />
                 </IconButton>
+
                 <Menu
                     anchorEl={profileAnchorEl}
                     open={Boolean(profileAnchorEl)}
@@ -138,6 +258,34 @@ const AdminNavbar = ({ onSearch }) => {
                     </MenuItem>
                 </Menu>
             </Box>
+
+            <Dialog open={openDialog} onClose={handleDialogClose} maxWidth="md" className="bounce-in-modal">
+                <DialogTitle sx={{ fontSize: '30px', fontWeight: 'bold' }}>Notification Details:</DialogTitle>
+                <DialogContent>
+                    {selectedNotification && (
+                        <Paper sx={{ p: 3, width: '100%', backgroundColor: '#041926' }} elevation={selectedNotification.isRead ? 1 : 4}>
+                            <Box display="flex" alignItems="center" mb={2}>
+                                <Avatar sx={{ bgcolor: '#3e4396', mr: 3 }}>{selectedNotification.message.charAt(0)}</Avatar>
+                                <Box>
+                                    <Typography variant="h6" sx={{ color: '#94e2cd', fontWeight: selectedNotification.isRead ? 'normal' : 'bold' }}>
+                                        {selectedNotification.message}
+                                    </Typography>
+                                    <Typography variant="body2" color="#e0e0e0">
+                                        {new Date(selectedNotification.createdAt).toLocaleString()}
+                                    </Typography>
+                                </Box>
+                            </Box>
+                            <Divider sx={{ mt: 2, mb: 2, bgcolor: "#e0e0e0" }} />
+                            <Typography variant="body2" color="#e0e0e0" sx={{ display: 'block', textAlign: 'right' }}>
+                                {selectedNotification.isRead ? 'Read' : 'Unread'}
+                            </Typography>
+                        </Paper>
+                    )}
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleDialogClose} color="primary">Close</Button>
+                </DialogActions>
+            </Dialog>
         </Box>
     );
 };
