6732807d33244c81a83302bb16280976253039c1
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-30 10:39:41 +0800

    updated

--- frontend/src/components/leadgen/LeadGenSidebar.jsx
+++ frontend/src/components/leadgen/LeadGenSidebar.jsx
@@ -118,7 +118,7 @@ const LeadGenSidebar = () => {
                             alt="profile-user"
                             width="100px"
                             height="100px"
-                            src={process.env.PUBLIC_URL + '/icon.png'}
+                            src={userLG.profileImage || process.env.PUBLIC_URL + '/icon.png'}
                             className="cursor-pointer rounded-full"
                         />
                     </div>
