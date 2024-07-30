cf4190f28ed7bb1f6104e758dcad325f7f34492f
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-07-30 10:33:26 +0800

    updated

--- frontend/src/components/leadgen/LeadGenNavbar.jsx
+++ frontend/src/components/leadgen/LeadGenNavbar.jsx
@@ -251,7 +251,7 @@ const LeadGenNavbar = ({ onSearch }) => {
                 >
                     <MenuItem>
                         <Typography>
-                            <Link to={`/viewprofile/${userLG._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <Link to={`/viewuser/${userLG._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
                                 Update Status
                             </Link>
                         </Typography>
